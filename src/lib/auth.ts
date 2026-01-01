8a7ae2c3cd733429929d971a0b3346d3983edf9b
Arsh Verma <arshverma.dev@gmail.com>
2026-01-02 01:15:47 +0530

    Create auth.ts

---
+++ src/lib/auth.ts
@@ -0,0 +1,57 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import prisma from "./prisma";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "Reg No." },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        if (!credentials?.username || !credentials?.password) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { username: credentials.username }
+        });
+
+        // In a real app, use bcrypt to compare passwords
+        if (user && user.password === credentials.password) {
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role
+          };
+        }
+        
+        return null;
+      }
+    })
+  ],
+  pages: {
+    signIn: '/',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.role = (user as any).role;
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        (session.user as any).id = token.id;
+        (session.user as any).role = token.role;
+      }
+      return session;
+    }
+  },
+  session: {
+    strategy: "jwt"
+  }
+};
