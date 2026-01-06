bfd2ad2c0341f4bca6bcce34169d45af19600d33
Arsh Verma <arshverma.dev@gmail.com>
2026-01-06 19:15:06 +0530

    Update LoginBox.tsx

--- src/components/LoginBox.tsx
+++ src/components/LoginBox.tsx
@@ -10,7 +10,7 @@ import { useRouter } from "next/navigation"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 
 export function LoginBox() {
