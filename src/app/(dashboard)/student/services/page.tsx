19879113de3d6b2b67c7aa9c97958d3796b7236e
Arsh Verma <arshverma.dev@gmail.com>
2026-01-17 14:43:18 +0530

    Update page.tsx

--- src/app/(dashboard)/student/services/page.tsx
+++ src/app/(dashboard)/student/services/page.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
-import { Calendar, FileText, Fingerprint, Plane } from "lucide-react"
+import { FileText, Fingerprint, Plane } from "lucide-react"
 
 export default function ServicesPage() {
   return (
