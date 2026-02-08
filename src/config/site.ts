bade6216085bbe2737b82918805e28202f341253
Arsh Verma <arshverma.dev@gmail.com>
2026-02-09 00:22:28 +0530

    Create site.ts

---
+++ src/config/site.ts
@@ -0,0 +1,20 @@
+export const siteConfig = {
+  name: "CampusHub",
+  description: "Synchronous University Intelligence & Governance Platform",
+  version: "2.1.0-alpha",
+  author: "Arsh Verma",
+  links: {
+    github: "https://github.com/ArshVermaGit/Vtop2.0",
+    twitter: "https://x.com/TheArshVerma",
+  },
+  contact: {
+    support: "arshverma.dev@gmail.com",
+    admin: "arshverma.dev@gmail.com",
+  },
+  theme: {
+    primary: "indigo",
+    dark: true,
+  }
+}
+
+export type SiteConfig = typeof siteConfig
