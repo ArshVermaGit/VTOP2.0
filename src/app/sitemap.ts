5b0cf98491c65609596e22d1535ed62bf4cf5b5f
Arsh Verma <arshverma.dev@gmail.com>
2026-02-09 00:21:11 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,13 +3,13 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://vtop.university',
+      url: 'https://campushub.university',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://vtop.university/dashboard',
+      url: 'https://campushub.university/dashboard',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
